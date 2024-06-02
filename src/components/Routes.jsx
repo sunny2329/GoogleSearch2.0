106f0b9723971de3f4db3a1e913420070f302a37
Sunny <sobhit230@gmail.com>
2024-06-03 01:55:09 +0530

    Initial Commit

---
+++ src/components/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+function Routes() {
+  return (
+    <div className='p-4 '>
+      <Switch>
+        <Route exact path='/'>
+            <Redirect to='/search'/>
+        </Route>
+        <Route exact path={['/search','/images','/news','/videos']}>
+    
+        </Route>
+      </Switch>
+    </div>
+  )
+}
+
+export default Routes
